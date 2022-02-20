8829ac1a1001ea3c1b8ac2e9553731acbef099e2
Neosvet <neosvet333@gmail.com>
2022-02-20 12:22:01 +0300

    realize activity

--- app/src/main/java/ru/neosvet/maket/MainActivity.kt
+++ app/src/main/java/ru/neosvet/maket/MainActivity.kt
@@ -2,10 +2,31 @@ package ru.neosvet.maket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.neosvet.maket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() = binding?.run{
+        bottomNavigationView.setOnItemSelectedListener {  item ->
+            when(item.itemId) {
+                R.id.bottom_view_home -> {
+
+                }
+                R.id.bottom_view_classes -> {
+
+                }
+            }
+            changeBottomItemSelected(item.itemId)
+            return@setOnItemSelectedListener true
+        }
     }
 }
